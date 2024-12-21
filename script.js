8bd975a298c21319910a5804ed9cadcb144c67ab
zsombi11-1 <zsombigame11@gmail.com>
2024-12-21 18:07:17 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const animatedElements = document.querySelectorAll('.animate');
-
-    animatedElements.forEach((element, index) => {
-        element.style.animationDelay = `${index * 0.1}s`;
-    });
-});
